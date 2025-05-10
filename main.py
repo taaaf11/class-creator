9bfbcabd300279f5ae1876cf4c1efb762bc7baea
taaaf11 <taafuuu@gmail.com>
2025-05-10 12:56:32 +0500

    add support for cmd line arguments

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from model import CppClass
 from transformer import MyTransformer
 # from utils import make_png, make_transformed_png
 
+from argparse import ArgumentParser
+
+
+def parse_args():
+    arg_parser = ArgumentParser(prog='class-creator', description="Create class using mermaid's classDiagram like slntax.")
+    arg_parser.add_argument(
+        "FILE_NAME",
+        help="Name of file containing code for generating class.",
+    )
+    arg_parser.add_argument(
+        "-o",
+        "--output",
+        default="class.cpp",
+        help="Name of output file. Optional, defaults to class.cpp.",
+    )
+    
+    return arg_parser.parse_args()
+
+
+cmd_args = parse_args()
 
 with open("grammar.lark") as file:
     parser = Lark(file, start="document")
 
+with open(cmd_args.FILE_NAME, "r") as f:
+    transformed_tree = MyTransformer().transform(parser.parse(f.read()))
 
-sentence = \
-"""\
-Classes:
-class Vehicle {
-   +int id
-}
-class Car: Vehicle {
-   +int id
-}
-"""
-
-o = MyTransformer().transform(parser.parse(sentence))
-
-with open("r.cpp", "w") as f:
+with open(cmd_args.output, "w") as f:
     class_: CppClass
-    for class_ in o.children:
+    for class_ in transformed_tree.children:
         f.write(class_.gen_stmt())
